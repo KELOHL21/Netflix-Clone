96ab75f37a27a60f2a950362452f18f8c2689eef
Kelsy Ohlsson <kelsyohlsson@gmail.com>
2022-05-09 22:58:40 +0200

    Updating Javascript

---
+++ Everything/Javascript/WatchList.js
@@ -0,0 +1,37 @@
+
+import { movieArray } from "./Movie_array.js";
+
+const { createApp } = window.Vue;
+
+const watchlist_Key= "watchlist_key";
+
+const Component = {
+    data() {
+      return {
+        movieList: movieArray,
+        watchListArray: [],
+      };
+    },
+    methods:{
+
+        removeFromWatchList(index) {
+            this.watchListArray = JSON.parse(localStorage.getItem(watchlist_Key));
+            this.watchListArray.splice(index, 0);
+            localStorage.setItem(watchlist_Key, JSON.stringify(this.watchListArray));
+          },
+    },
+    //Change
+    mounted() {
+      if (!localStorage.getItem(watchlist_Key)) {
+        let newArray = [];
+        localStorage.setItem(watchlist_key, JSON.stringify(newArray));
+      }
+      this.watchListArray = JSON.parse(localStorage.getItem(watchlist_Key));
+    },
+  };
+
+//Mouting App
+window.addEventListener("DOMContentLoaded", () => {
+  const app = createApp(Component);
+  app.mount("#myApp");
+})
